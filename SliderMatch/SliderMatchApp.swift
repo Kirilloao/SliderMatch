06714073da4988bad87096e86ae4e84bd753c1d4
Kirilloao <kirilltaraturin747@gmail>
2023-08-25 20:28:18 +0200

    Initial Commit

---
+++ SliderMatch/SliderMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderMatchApp.swift
+//  SliderMatch
+//
+//  Created by Kirill Taraturin on 25.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

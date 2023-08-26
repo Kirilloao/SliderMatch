5c044f519dd6a7cbb3b426abb10bdc43147e02c1
Kirilloao <kirilltaraturin747@gmail>
2023-08-26 15:15:32 +0200

    update

---
+++ SliderMatch/TargetValueStore.swift
@@ -0,0 +1,12 @@
+//
+//  TargetValueStore.swift
+//  SliderMatch
+//
+//  Created by Kirill Taraturin on 26.08.2023.
+//
+
+import Foundation
+
+class TargetValueStore: ObservableObject {
+    @Published var targetValue = 0
+}
